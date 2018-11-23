786180783ab3e3b7ab16f4213071a05687764e5b
Cruzzzzz <45172092+Cruzzzzz@users.noreply.github.com>
2018-11-23 10:09:49 +0100

    Add files via upload

---
+++ grade calculator.py
@@ -0,0 +1,21 @@
+print('     GRADE CALCULATOR    ')
+
+score = int(input('what is your score?'))
+
+if score >= 70 <=100:
+    print('your grade is A')
+    
+elif score >= 60 <=69:
+    print('your grade is B')
+    
+elif score >= 50 <=59:
+    print('your grade is C')
+    
+elif score >= 45 <=49:
+    print('your grade is D')
+    
+elif score >= 40 <=44:
+    print('your grade is D')
+    
+elif score >= 0 <=39:
+    print(' opps your grade is F ')
